93c04f4173c6ca09da9ef50e8f4cb921890795d4
James Vu <jamestvu@users.noreply.github.com>
2020-02-08 16:00:26 -1000

    Fixed extro intro page css.

--- src/components/ExtroVsIntro.vue
+++ src/components/ExtroVsIntro.vue
@@ -1,21 +1,24 @@
 <template>
   <q-page class="flex flex-center extro-intro">
-    <div class="col-12">
-      <div class="row justify-center" align="center" style="margin-bottom: 50px"><h3>Extroverts Vs. Introverts</h3></div>
+    <div class="col">
+      <div class="row justify-center" align="center" style="margin-bottom: 100px"><h3>Extroverts Vs. Introverts</h3></div>
       <div class="row justify-center">
-        <div class="col-3">
-          <img class="row ei-img" alt="introverts" src="../assets/introverts.png">
+        <div class="col-4">
           <div class="row justify-center">
+            <img class="row ei-img" alt="introverts" src="../assets/introverts.png">
+          </div>
+          <div class="row justify-center" style="height: 75px">
             <h3>32%</h3>
           </div>
           <div class="row justify-center">
             <h5>Extroverts</h5>
           </div>
         </div>
-        <div class="col-6"></div>
-        <div class="col-3">
-          <img class="row ei-img" alt="extroverts" src="../assets/extrovert.png">
+        <div class="col-4">
           <div class="row justify-center">
+            <img class="row ei-img" alt="extroverts" src="../assets/extrovert.png">
+          </div>
+          <div class="row justify-center" style="height: 75px">
             <h3>68%</h3>
           </div>
           <div class="row justify-center">
@@ -31,6 +34,9 @@
   .extro-intro {
     background-color: #2d92cc;
     color: white;
+  }
+  .ei-rhombus {
+    -webkit-transform: translateY(-20px) translateX(3px) rotate(10deg);
   }
 
   .ei-img {
