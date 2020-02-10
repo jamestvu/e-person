f087c3938a47a4cf6fa7af5a3b2cd25af6c16233
Ashley Fagaragan <asfagaragan@gmail.com>
2020-02-09 17:54:41 -1000

    managers changed to click

--- src/views/Managers.vue
+++ src/views/Managers.vue
@@ -1,107 +1,101 @@
 <template>
-  <q-page class="q-pa-md flex flex-center text-center text-white">
-    <div class="text-h3">Manager Personalities</div>
-    <div class="row q-gutter-md">
-      <template v-for="(manager, index) in managers">
-        <div class="col text-black" :key="index">
-          <q-card>
-            <q-card-section>
-              <img src="../assets/profile-placeholder.png"
-                   style="width: 250px; height: 250px !important"
-                   @mouseenter="manager.imageToggle = !manager.imageToggle"
-                   v-if="!manager.imageToggle"
-              >
-              <div v-if="manager.imageToggle">
-                <img :src="manager.image" style="height: 220px !important"
-                     @mouseleave="manager.imageToggle = !manager.imageToggle"
-                >
-                <div class="text-subtitle1">{{ manager.trait }}</div>
-              </div>
-            </q-card-section>
-            <q-card-section>
-              <div class="text-h5">{{ manager.name }}</div>
-              <div class="text-caption">{{ manager.nickname }}</div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </template>
-    </div>
-    <div class="text-center">DISCLAIMER: Only Tom submitted his results. (and Dean obviously)</div>
+  <q-page class="q-pa-md text-center text-white" style="background-color: #5F5980">
+      <div class="text-h3 q-mt-xl">Manager Personalities</div>
+      <div class="text-subtitle2">Click on any manager to find out what personality they are!</div>
+      <div class="row wrap" style="margin-top: 5em">
+        <template v-for="(manager, index) in managers">
+          <div class="col-md-2 col-xs-4 text-black q-mb-md" :key="index">
+            <q-card class="full-height q-ma-xs">
+              <q-card-section>
+                <div style="margin: 0 auto">
+                  <img :src="manager.original"
+                       style="width: 100%; height: 100%; border-radius: 50%; border: 2px solid black"
+                       @click="manager.imageToggle = !manager.imageToggle"
+                       v-if="!manager.imageToggle">
+                  <div v-if="manager.imageToggle">
+                    <img :src="manager.image"
+                         @click="manager.imageToggle = !manager.imageToggle"
+                    >
+                    <div class="text-subtitle1">{{ manager.trait }}</div>
+
+                  </div>
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <div class="text-h5">{{ manager.name }}</div>
+                <div class="text-caption">{{ manager.nickname }}</div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </template>
+      </div>
+    <br>
+      <div class="text-center">DISCLAIMER: Only Tom submitted his results. (and Dean obviously)</div>
   </q-page>
 </template>
 
 <style>
-  body {
-    background-color: #5F5980;
-  }
-
-  #managers .personality {
-    border-radius: 50%;
-  }
-
-  #managers .text-subtitle1 {
-    height: 30px;
-  }
-
-  #managers img {
-    height: 250px;
-    width: 250px;
-  }
 </style>
 
 <script>
 
-export default {
-  name: 'Managers.vue',
+  export default {
+    name: 'Managers.vue',
 
     data() {
       return {
-          managers: [
-            {
-              name: 'Tom',
-              nickname: 'Big T',
-              imageToggle: false,
-              trait: 'Consul (ESFJ-A)',
-              image: 'https://static.neris-assets.com/images/personality-types/famous/sentinels_ESFJ_bill_clinton.svg?v=3'
-            },
-            {
-              name: 'Joel',
-              nickname: 'JoJo',
-              imageToggle: false,
-              trait: 'Defender (ISFJ-A)',
-              image: 'https://static.neris-assets.com/images/personality-types/famous/sentinels_ISFJ_captain_america.svg?v=3'
-            },
-            {
-              name: 'Rex',
-              nickname: 'Dino',
-              imageToggle: false,
-              trait: 'Logician (INTP-A)',
-              image: 'https://static.neris-assets.com/images/personality-types/famous/analysts_INTP_neo_the_matrix.svg?v=3'
-            },
-            {
-              name: 'Steve',
-              nickname: 'Sakats',
-              imageToggle: false,
-              trait: 'Campaigner (ENFP-A)',
-              image: 'https://static.neris-assets.com/images/personality-types/famous/diplomats_ENFP_willy_wonka_charlie_and_the_chocolate_factory.svg?v=3'
-            },
-            {
-              name: 'Bhanu',
-              nickname: 'The Stache',
-              imageToggle: false,
-              trait: 'Entertainer (ESFP-A)',
-              image: 'https://static.neris-assets.com/images/personality-types/famous/explorers_ESFP_steve_irwin.svg?v=3'
-            },
-            {
-              name: 'Dean',
-              nickname: 'Dean the Destroyer',
-              imageToggle: false,
-              trait: 'Destroyer (DEAN-S)',
-              image: 'https://static.neris-assets.com/images/personality-types/famous/analysts_INTP_bruce_banner.svg?v=3'
-            },
-          ]
+        managers: [
+          {
+            name: 'Tom',
+            nickname: 'Big T',
+            imageToggle: false,
+            trait: 'Consul (ESFJ-A)',
+            original: require('@/assets/managers/tom.png'),
+            image: 'https://static.neris-assets.com/images/personality-types/famous/sentinels_ESFJ_bill_clinton.svg?v=3'
+          },
+          {
+            name: 'Joel',
+            nickname: 'JoJo',
+            imageToggle: false,
+            trait: 'Defender (ISFJ-A)',
+            original: require('@/assets/managers/joel.jpeg'),
+            image: 'https://static.neris-assets.com/images/personality-types/famous/sentinels_ISFJ_captain_america.svg?v=3'
+          },
+          {
+            name: 'Rex',
+            nickname: 'Dino',
+            imageToggle: false,
+            trait: 'Logician (INTP-A)',
+            original: require('@/assets/managers/rex.png'),
+            image: 'https://static.neris-assets.com/images/personality-types/famous/analysts_INTP_neo_the_matrix.svg?v=3'
+          },
+          {
+            name: 'Steve',
+            nickname: 'Sakats',
+            imageToggle: false,
+            trait: 'Campaigner (ENFP-A)',
+            original: require('@/assets/managers/steve.png'),
+            image: 'https://static.neris-assets.com/images/personality-types/famous/diplomats_ENFP_willy_wonka_charlie_and_the_chocolate_factory.svg?v=3'
+          },
+          {
+            name: 'Bhanu',
+            nickname: 'The Stache',
+            imageToggle: false,
+            trait: 'Entertainer (ESFP-A)',
+            original: require('@/assets/managers/bhanu.png'),
+            image: 'https://static.neris-assets.com/images/personality-types/famous/explorers_ESFP_steve_irwin.svg?v=3'
+          },
+          {
+            name: 'Dean',
+            nickname: 'Dean the Destroyer',
+            imageToggle: false,
+            trait: 'Destroyer (DEAN-S)',
+            original: require('@/assets/managers/dean.png'),
+            image: 'https://static.neris-assets.com/images/personality-types/famous/analysts_INTP_bruce_banner.svg?v=3'
+          },
+        ]
       }
     },
-}
+  }
 
 </script>
